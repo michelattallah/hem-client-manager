050c18a6dc64632da7616b3983ad7061e7fb2a1b
Michel Melad <michelmelad@hotmail.com>
2020-03-03 16:01:29 -0500

    Init API

---
+++ api/app/Http/Resources/Clients/ClientsResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Clients;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClientsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'name'          => $this->name,
+            'description'   => $this->description,
+            'logo'          => $this->logo
+        ];
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::prefix('/s/')->group(function () {
+
+    Route::post('clients' , 'ClientsController@index');
+
+
+});
+
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
